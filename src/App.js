2ec9368c1689594f4d4b265b4b288a2d9ff2127a
Juan Neira <jincastillo992>
2022-02-11 01:47:10 -0300

    Created Login for Spotify Clone App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Login from './components/login/Login.jsx';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+import Player from './components/player/Player.jsx'
+import { useDataLayerValue } from './components/data_layer/DataLayer.jsx';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    if(_token) {
+      setToken(_token);
+      spotify.setAccessToken(_token); 
+      spotify.getMe().then((user) => {
+      });
+    }
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {
+        token ? (<Player />) : (<Login />)
+      }
     </div>
   );
 }
---
+++ src/components/player/Player.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Player() {
+  return (
+    <div>
+      <h1>Welcome to Spotify-Clone</h1>
+    </div>
+  )
+}
+
+export default Player
